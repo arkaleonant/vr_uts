32559e77435040a4289e5a03e3f996c33ce41f08
Arka Arifiandi Leonanta <arkaarif69@gmail.com>
2020-10-26 15:56:03 +0700

    add project vr uts
    add project vr uts

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PindahHalaman(string halaman)
+    {
+        SceneManager.LoadScene(halaman);
+    }
+
+    // Update is called once per frame
+    public void KeluarAplikasi()
+    {
+        Application.Quit();
+    }
+}
